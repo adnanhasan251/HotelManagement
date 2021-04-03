f9fac7e59c3cd796b8c9d6698b64e3427893db2e
adnanhasan251 <68420989+adnanhasan251@users.noreply.github.com>
2021-04-03 12:46:08 +0530

    Update main.py
    Colors added for better UI

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from __future__ import print_function, absolute_import, with_statement
+#from __future__ import print_function, absolute_import, with_statement
 import pandas as pd
+import stdiomask
 import cfonts
 import db_connect as dbc
 from tabulate import tabulate
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 dbc.connect('hotel_t1')
 
@@ -10,61 +23,60 @@ cfonts.say("WELCOME TO HOTEL CALIFORNIA", font='chrome', colors=['candy', 'candy
 
 def guest():
     dbc.create('guest_t2', "'fname', 'lname', 'phone', 'email', 'address', 'adults', 'child', 'stay', 'room_no', 'wifi'")
-    #check_room()
     # personal info
-    fname = input("First Name:  ")
-    lname = input("Last Name:  ")
+    fname = input(bcolors.OKGREEN+"First Name:  "+bcolors.ENDC)
+    lname = input(bcolors.OKGREEN+"Last Name:  "+bcolors.ENDC)
     # contact
     while True:
-        phone = input("Phone Number:  ")
-        if (phone.isnumeric()) and (8<len(phone)<13) and (" " not in phone) and (phone.startswith("9") or phone.startswith("7")  or phone.startswith("8") or phone.startswith("91")):
+        phone = input(bcolors.OKGREEN+"Phone Number:  "+bcolors.ENDC)
+        if (phone.isnumeric()) and (8<len(phone)<13) and (" " not in phone):
             break
         else:
-            print("Invalid phone number")
+            print(bcolors.FAIL+"Invalid phone number"+bcolors.ENDC)
 
  
 
     while True:
-        email = input("Email:  ")
+        email = input(bcolors.OKGREEN+"Email:  "+bcolors.ENDC)
         if "@" in email:
             a=email.partition("@")
             if len(a[0])!=0 and len(a[2])!=0 and "@" not in a[0] and "@" not in a[2]:
                 break
             else:
-                print("Invalid email")
+                print(bcolors.FAIL+"Invalid email"+bcolors.ENDC)
         else:
-            print("Invalid email")
+            print(bcolors.FAIL+"Invalid email"+bcolors.ENDC)
 
 
             
     while True:
-        address = input("Guest's Address:  ")
+        address = input(bcolors.OKGREEN+"Guest's Address:  "+bcolors.ENDC)
         if address.isalnum() and len(address)!=0:
             break
         else:
-            print("Invalid")
+            print(bcolors.FAIL+"Invalid"+bcolors.ENDC)
     # reservation info
     while True:
-        adults = input("Number of Adults:  ")
+        adults = input(bcolors.OKGREEN+"Number of Adults:  "+bcolors.ENDC)
         if int(adults)>=0 and adults.isnumeric():
             break
         else:
-            print("Invalid number")
+            print(bcolors.FAIL+"Invalid number"+bcolors.ENDC)
     while True:
-        child = input("Number of Children:  ")
+        child = input(bcolors.OKGREEN+"Number of Children:  "+bcolors.ENDC)
         if int(child)>=0 and child.isnumeric():
             break
         else:
-            print("Invalid number")
+            print(bcolors.FAIL+"Invalid number"+bcolors.ENDC)
     while True:
-        stay = input("Number of Days of stay:  ")
+        stay = input(bcolors.OKGREEN+"Number of Days of stay:  "+bcolors.ENDC)
         if int(stay)>=0 and stay.isnumeric():
             break
         else:
-            print("Invalid number")
-    wifi=input("Wifi access required (y/n):  ").upper()
+            print(bcolors.FAIL+"Invalid number"+bcolors.ENDC)
+    wifi=input(bcolors.OKGREEN+"Wifi access required (y/n):  "+bcolors.ENDC).upper()
     # room
-    room_no = input("Enter the room number to reserve:  ")
+    room_no = input(bcolors.OKGREEN+"Enter the room number to reserve:  "+bcolors.ENDC)
     #insert guest details in database
     data="'"+fname+"', '"+lname+"', "+phone+", '"+email+"', '"+address+"', "+adults+", "+child+", "+stay+", "+room_no+", '"+wifi+"'"
     dbc.insert("guest_t2", data)
@@ -72,6 +84,7 @@ def guest():
     upd_cond = "room_no="+room_no
     dbc.update("rooms", "status='R'", upd_cond)
 
+    #confirmation for room allotment
     chosen = [
                 ["First name", fname],
                 ["Last name", lname],
@@ -85,39 +98,39 @@ def guest():
 
              ]
 
-    print("\n\n***********  ROOM ALLOTMENT ************\n\n")
+    print(bcolors.BOLD+"\n\n***********  ROOM ALLOTMENT ************\n\n"+bcolors.ENDC)
     print(tabulate(chosen, tablefmt="fancy_grid"))
-    con_trans = input("Do you want to confirm reservation ? [y/n]")
+    con_trans = input(bcolors.OKGREEN+"Do you want to confirm reservation ? [y/n]"+bcolors.ENDC)
     if con_trans in ["y", "Y", "Yes", "YES"]:
-        print("\n\nReservation Successful\n\n")
+        print(bcolors.OKCYAN+"\n\nReservation Successful\n\n"+bcolors.ENDC)
 
-    y_n = input("\nDo you want to book another room ? [y/n]\n")
+    y_n = input(bcolors.OKGREEN+"\nDo you want to book another room ? [y/n]\n"+bcolors.ENDC)
     if y_n in ["n", "N", "No"]:
-        print("Thank you")
+        print(bcolors.OKCYAN+"Thank you"+bcolors.ENDC)
         global flag
         flag = False
 
 def check_room():
-    beds = input("Number of beds:  ")
-    ac = input("AC required (y/n):  ").upper()
-    tv = input("TV required (y/n):  ").upper()
+    beds = input(bcolors.OKGREEN+"Number of beds:  "+bcolors.ENDC)
+    ac = input(bcolors.OKGREEN+"AC required (y/n):  "+bcolors.ENDC).upper()
+    tv = input(bcolors.OKGREEN+"TV required (y/n):  "+bcolors.ENDC).upper()
     _filter = "beds="+beds+" AND ac="+"'"+ac+"'"+" AND tv="+"'"+tv+"'"+" AND status='NR'"
     available = dbc.get("rooms", "room_no", _filter)
-    print("ROOMS AVAILABLE:")
+    print(bcolors.UNDERLINE+"ROOMS AVAILABLE:"+bcolors.ENDC)
     for Number in available:
         print(Number[0])
-    print("Please select an option:")
-    print("1. Reserve a room")
-    print("2. Check another room")
-    opt=int(input("(1/2):  "))
+    print(bcolors.OKCYAN+'''Please select an option
+    1. Reserve a room
+    2. Check another room'''+bcolors.ENDC)
+    opt=int(input(bcolors.OKGREEN+"(1/2):  "+bcolors.ENDC))
     if opt==1:
         guest()
     elif opt==2:
         check_room()
 
 def check_out():
-    roomno=input("Enter the room number to check out:  ")
-    stay=int(input("Days stayed?:  "))
+    roomno=input(bcolors.OKGREEN+"Enter the room number to check out:  "+bcolors.ENDC)
+    stay=int(input(bcolors.OKGREEN+"Days stayed?:  "+bcolors.ENDC))
     dat="fname, lname, phone, room_no, wifi"
     _filter="room_no="+roomno
     roominf=dbc.get("guest_t2",dat,_filter)
@@ -134,6 +147,7 @@ def check_out():
     ac=facility[0][1]
     tv=facility[0][2]
 
+    #generating bill
 
     item1="'"+ str(beds)+" bed" +"'"
     _filter3="item="+item1
@@ -176,18 +190,18 @@ def check_out():
 
              ]
 
-    co=input("Do you want to check out? (y/n)")
+    co=input(bcolors.WARNING+"Do you want to check out? (y/n)"+bcolors.ENDC)
     if co.lower()=="y":
-        #BILL
-        print("\n\n***********  INVOICE ************\n\n")
+        #Print BILL
+        print(bcolors.BOLD+"\n\n***********  INVOICE ************\n\n"+bcolors.ENDC)
         print(tabulate(inv, tablefmt="fancy_grid"))
-        con_trans = input("Do you want to confirm transaction ? [y/n]")
+        con_trans = input(bcolors.OKGREEN+"Do you want to confirm transaction ? [y/n]"+bcolors.ENDC)
         if con_trans in ["y", "Y", "Yes", "YES"]:
-            print("\n\nTransaction Successful\n\n")
+            print(bcolors.OKCYAN+"\n\nTransaction Successful\n\n"+bcolors.ENDC)
 
-        y_n = input("\nDo you want to book another room ? [y/n]\n")
+        y_n = input(bcolors.OKGREEN+"\nDo you want to book another room ? [y/n]\n"+bcolors.ENDC)
         if y_n in ["n", "N", "No"]:
-            print("Thank you")
+            print(bcolors.OKCYAN+"Thank you"+bcolors.ENDC)
             global flag
             flag = False
         cond="room_no="+roomno
@@ -203,28 +217,28 @@ def room_status():
     guests=dbc.seetable("guest_t2")
     room_ = pd.DataFrame(rooms, columns=['Room no', 'Status','No of Beds','AC', 'TV'])
     guest_= pd.DataFrame(guests, columns=['First name', 'Last name', 'Phone No', 'Email', 'Address', 'No of Adults', 'No of Child', 'Days of Stay', 'Room No', 'Wifi Access'])
-    print("Please select an option:")
-    print("1. See all the room status")
-    print("2. See guests details (admin only)")
-    print("3. Back to Main Menu")
-    opt=int(input("Enter (1/2/3):  "))
+    print(bcolors.OKCYAN+'''Please select an option:
+    1. See all the room status
+    2. See guests details (admin only)
+    3. Back to Main Menu'''+bcolors.ENDC)
+    opt=int(input(bcolors.OKGREEN+"Enter (1/2/3):  "+bcolors.ENDC))
     if opt==1:
-        print("ROOMS:")
+        print(bcolors.UNDERLINE+"ROOMS:"+bcolors.ENDC)
         print(room_)
     elif opt==2:
         if adm==True:
-            print("GUESTS:")
+            print(bcolors.UNDERLINE+"GUESTS:"+bcolors.ENDC)
             print(guest_)
         else:
-            print("You need admin priviledge to view the data")
-            opt=input("Do you want to login as admin? (y/n)").lower()
+            print(bcolors.WARNING+"You need admin priviledge to view the data"+bcolors.ENDC)
+            opt=input(bcolors.OKGREEN+"Do you want to login as admin? (y/n)"+bcolors.ENDC).lower()
             if opt=='y':
                 log = False
                 login()
     elif opt==3:
-        print("Main Menu:")
+        print(bcolors.UNDERLINE+"Main Menu:"+bcolors.ENDC)
     else:
-        print("Please select from one of the given options!")
+        print(bcolors.FAIL+"Please select from one of the given options!"+bcolors.ENDC)
         room_status()
 
 
@@ -233,15 +247,15 @@ def rate():
     rate=dbc.seetable("rate")
     rate_=pd.DataFrame(rate, columns=['Item', 'Rate per day'])
     print(rate_)
-    opt=input("Do you want to change rates (admin only)? (y/n)").lower()
+    opt=input(bcolors.OKGREEN+"Do you want to change rates (admin only)? (y/n)"+bcolors.ENDC).lower()
     if opt=='y':
         if adm==True:
-            print('''Which rate do you want to change?
+            print(bcolors.OKCYAN+'''Which rate do you want to change?
             1. Charge of 'n' bed per day
-            2. Charge of AC/TV/Wifi per day''')
-            opt2=int(input("Enter (1/2):  "))
+            2. Charge of AC/TV/Wifi per day'''+bcolors.ENDC)
+            opt2=int(input(bcolors.OKGREEN+"Enter (1/2):  "+bcolors.ENDC))
             if opt2==1:
-                nbed=int(input("Enter the number of beds (1/2/3) of which you wish to change rates:  "))
+                nbed=int(input(bcolors.OKGREEN+"Enter the number of beds (1/2/3) of which you wish to change rates:  "+bcolors.ENDC))
                 item="item='"+str(nbed)+" bed'"
                 crate=dbc.get("rate","rpd", item)[0][0]
                 print("Current rate is Rs."+str(crate))
@@ -250,20 +264,20 @@ def rate():
                 dbc.update('rate', uptr, item)
                 print("The new is set to Rs."+str(nrate))
             if opt2==2:
-                print('''Please select one of the options:
-                1. TV, 2. AC, 3. Wifi''')
-                opt3=int(input("Enter (1/2/3:  "))
+                print(bcolors.OKCYAN+'''Please select one of the options:
+                1. TV, 2. AC, 3. Wifi'''+bcolors.ENDC)
+                opt3=int(input(bcolors.OKGREEN+"Enter (1/2/3:  "+bcolors.ENDC))
                 items=['tv', 'ac', 'wifi']
                 item="item='"+items[(opt3)-1]+"'"
                 crate=dbc.get("rate","rpd", item)[0][0]
                 print("Current rate is Rs."+str(crate))
-                nrate=int(input("Enter the new rate (Rs.):  "))
+                nrate=int(input(bcolors.OKGREEN+"Enter the new rate (Rs.):  "+bcolors.ENDC))
                 uptr="rpd="+str(nrate)
                 dbc.update('rate', uptr, item)
-                print("The new is set to Rs."+str(nrate))
+                print(bcolors.OKCYAN+"The new is set to Rs."+str(nrate)+bcolors.ENDC)
         else:
-                print("You need admin priviledge to change the rates")
-                opt=input("Do you want to login as admin? (y/n)").lower()
+                print(bcolors.WARNING+"You need admin priviledge to change the rates"+bcolors.ENDC)
+                opt=input(bcolors.OKGREEN+"Do you want to login as admin? (y/n)"+bcolors.ENDC).lower()
                 if opt=='y':
                     log = False
                     login()
@@ -271,13 +285,13 @@ def rate():
 def interf():
     global log
     global adm
-    print("Please choose a option:")
-    print("1. Reserve a room")
-    print("2. Check out")
-    print("3. Check all room status")
-    print("4. Check rates")
-    print("5. Logout")
-    initial_opt=int(input("Enter (1/2/3/4/5):  "))
+    print(bcolors.OKCYAN+'''Please choose a option:
+    1. Reserve a room
+    2. Check out
+    3. Check all room status
+    4. Check rates
+    5. Logout'''+bcolors.ENDC)
+    initial_opt=int(input(bcolors.OKGREEN+"Enter (1/2/3/4/5):  "+bcolors.ENDC))
     if initial_opt==1:
         check_room()
     elif initial_opt==2:
@@ -290,7 +304,7 @@ def interf():
         log = False
         adm = False
     else:
-        print("Please select from one of the given options!")
+        print(bcolors.FAIL+"Please select from one of the given options!"+bcolors.ENDC)
 
 
 log=False
@@ -301,29 +315,29 @@ def login():
     if log==True:
         interf()
     else:
-        user=input("Enter username:  ")
-        passwd=input("Enter password:  ")
+        user=input(bcolors.OKGREEN+"Enter username:  "+bcolors.ENDC)
+        passwd=stdiomask.getpass(prompt=bcolors.OKGREEN+"Enter password:  "+bcolors.ENDC)
         auser=dbc.seetable("cred")[0][0]
         apass=dbc.seetable("cred")[0][1]
         suser=dbc.seetable("cred")[1][0]
         spass=dbc.seetable("cred")[1][1]
 
 
-        #user=staff passwd=qazxsw@hotel@calif
+        #user=staff passwd=hotelstaff123
         if user==suser: 
             if passwd==spass:
                 log = True
             else:
-                print("Values do not match the record, try again!")
-        #user=admin passwd=admin@pwd@753
+                print(bcolors.FAIL+"Values do not match the record, try again!"+bcolors.ENDC)
+        #user=admin passwd=hoteladmin123
         elif user==auser:
             if passwd==apass:
                 log = True
                 adm = True
             else:
-                print("Values do not match the record, try again!")
+                print(bcolors.FAIL+"Values do not match the record, try again!"+bcolors.ENDC)
         else:
-            print("Values do not match the record, try again!")
+            print(bcolors.FAIL+"Values do not match the record, try again!"+bcolors.ENDC)
             log = False
 
 flag = True
